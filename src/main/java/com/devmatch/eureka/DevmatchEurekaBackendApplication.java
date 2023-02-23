aaa2bb3b5f2d923f6d2949847236b57892718314
gbin <gbin@brasilseg.com.br>
2023-02-22 23:00:39 -0300

    First commit

---
+++ src/main/java/com/devmatch/eureka/DevmatchEurekaBackendApplication.java
@@ -0,0 +1,15 @@
+package com.devmatch.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DevmatchEurekaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmatchEurekaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmatch/eureka/DevmatchEurekaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmatch.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmatchEurekaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
